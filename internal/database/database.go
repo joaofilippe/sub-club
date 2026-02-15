85740f34f51735fe37604ca563b3e0bc1361bc61
Joao Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2026-02-15 14:49:20 -0300

    feat: Add initial database connection utility and environment-based configuration loading.

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,18 +5,16 @@ import (
 	"fmt"
 	"time"
 
-	_ "github.com/jackc/pgx/v5/stdlib" // Standard library driver
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 )
 
-// NewConnection creates a connection pool to the database
 func NewConnection(ctx context.Context, connectionString string) (*sqlx.DB, error) {
 	db, err := sqlx.ConnectContext(ctx, "pgx", connectionString)
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to database: %w", err)
 	}
 
-	// Optimization: Configure pool settings here if needed
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(2)
 	db.SetConnMaxLifetime(time.Hour)
