fdc1808f574546a0160f576234a366bc580f3b1b
Joao Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2026-02-15 14:32:49 -0300

    feat: add initial database connection utility with pooling configuration

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,31 +5,21 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/stdlib" // Standard library driver
+	"github.com/jmoiron/sqlx"
 )
 
 // NewConnection creates a connection pool to the database
-func NewConnection(ctx context.Context, connectionString string) (*pgxpool.Pool, error) {
-	config, err := pgxpool.ParseConfig(connectionString)
+func NewConnection(ctx context.Context, connectionString string) (*sqlx.DB, error) {
+	db, err := sqlx.ConnectContext(ctx, "pgx", connectionString)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse connection string: %w", err)
+		return nil, fmt.Errorf("unable to connect to database: %w", err)
 	}
 
 	// Optimization: Configure pool settings here if needed
-	config.MaxConns = 10
-	config.MinConns = 2
-	config.MaxConnLifetime = time.Hour
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(2)
+	db.SetConnMaxLifetime(time.Hour)
 
-	pool, err := pgxpool.NewWithConfig(ctx, config)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create connection pool: %w", err)
-	}
-
-	// Verify connection
-	if err := pool.Ping(ctx); err != nil {
-		pool.Close()
-		return nil, fmt.Errorf("unable to ping database: %w", err)
-	}
-
-	return pool, nil
+	return db, nil
 }
