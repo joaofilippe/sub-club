6b438a4c2d0fcfc93c0d1b96bb45f5031f685713
Joao Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2026-02-23 20:55:59 -0300

    feat: add GetByType on Repository interface

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -8,6 +8,7 @@ type Repository interface {
 	GetByID(ctx context.Context, id string) (*User, error)
 	GetByEmail(ctx context.Context, email string) (*User, error)
 	GetByRole(ctx context.Context, role UserRole) ([]*User, error)
+	GetByType(ctx context.Context, userType UserType) ([]*User, error)
 	Update(ctx context.Context, user *User) error
 	Delete(ctx context.Context, id string) error
 	List(ctx context.Context) ([]*User, error)
