42d7dd6fbaab010f4c92d073971eaef84fc34c68
Joao Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2026-02-26 09:40:24 -0300

    feat: create Connection struct

--- internal/infra/database/database.go
+++ internal/infra/database/database.go
@@ -7,10 +7,18 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
+	"github.com/joaofilippe/subclub/internal/config"
 )
 
-func NewConnection(ctx context.Context, driverName, connectionString string) (*sqlx.DB, error) {
-	db, err := sqlx.ConnectContext(ctx, driverName, connectionString)
+var connection *Connection
+
+type Connection struct {
+	appConfig *config.Config
+	db *sqlx.DB
+}
+
+func NewConnection(ctx context.Context, appConfig *config.Config) (*Connection, error) {
+	db, err := sqlx.ConnectContext(ctx, appConfig.DatabaseDriver, appConfig.DatabaseURL)
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to database: %w", err)
 	}
@@ -19,5 +27,14 @@ func NewConnection(ctx context.Context, driverName, connectionString string) (*s
 	db.SetMaxIdleConns(2)
 	db.SetConnMaxLifetime(time.Hour)
 
-	return db, nil
+	connection = &Connection{
+		appConfig: appConfig,
+		db: db,
+	}
+
+	return connection, nil
+}
+
+func (c *Connection) GetDB() *sqlx.DB {
+	return c.db
 }
